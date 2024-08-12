6701704760227080cfdd1992e9bf73729b280358
koichini <nishikawa.ko1@gmail.com>
2024-08-12 21:54:12 +0900

    update tabstep etc...

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "gopls" }
+local servers = { "html", "cssls", "tsserver", "gopls", "eslint" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
@@ -17,11 +17,11 @@ for _, lsp in ipairs(servers) do
 end
 
 -- configuring single server, example: typescript
--- lspconfig.tsserver.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
+lspconfig.tsserver.setup {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+}
 
 -- Configure gopls specifically
 lspconfig.gopls.setup {
--- lua/options.lua
+++ lua/options.lua
@@ -1,18 +1,19 @@
 require "nvchad.options"
 
 -- add yours here!
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.relativenumber = true
+-- vim.opt.tabstop = 4
+-- vim.opt.shiftwidth = 4
 
 -- ファイルタイプ指定の設定
--- vim.api.nvim_create_autocmd("FileType", {
---   pattern = "go",
---   callback = function()
---     vim.opt.tabstop = 4
---     vim.opt.shiftwidth = 4
---     vim.opt.expandtab = false
---   end,
--- })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "go",
+  callback = function()
+    vim.opt.tabstop = 4
+    vim.opt.shiftwidth = 4
+    vim.opt.expandtab = false
+  end,
+})
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
