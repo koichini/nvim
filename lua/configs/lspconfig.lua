a196034eb4a400a18d27debede7c2bab1559026e
koichini <nishikawa.ko1@gmail.com>
2024-08-12 10:47:59 +0900

    go settings

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "gopls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
@@ -22,3 +22,27 @@ end
 --   on_init = nvlsp.on_init,
 --   capabilities = nvlsp.capabilities,
 -- }
+
+-- Configure gopls specifically
+lspconfig.gopls.setup {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  settings = {
+    gopls = {
+      gofumpt = true,
+      staticcheck = true,
+      analyses = {
+        unusedparams = true,
+        unreachable = true,
+      },
+      usePlaceholders = true,
+      codelenses = {
+        generate = true,
+        gc_details = true,
+      },
+      -- Formatting options are set globally for the language server, not individually.
+      -- Use the Go tools (e.g., gofmt) to handle formatting.
+    },
+  },
+}
