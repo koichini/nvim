adb0f27d78d3bc3ed5aafed506585dc0a6a621ed
koichini <nishikawa.ko1@gmail.com>
2024-08-12 23:21:53 +0900

    add: buffer change alt + number

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -9,3 +9,10 @@ map("i", "jk", "<ESC>")
 
 map({ "n", "v" }, "<leader>gg", ":LazyGit<CR>", { desc = "Open LazyGit" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- GotoTab: Alt+1 ~ Alt+9 tab change
+for i = 1, 9, 1 do
+  vim.keymap.set("n", string.format("<A-%s>", i), function()
+    vim.api.nvim_set_current_buf(vim.t.bufs[i])
+  end)
+end
